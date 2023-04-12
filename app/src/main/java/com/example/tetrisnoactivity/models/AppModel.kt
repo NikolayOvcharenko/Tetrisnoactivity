37fa07660b8a36d940d760d428a773e037d0d873
Nikolay <unicorn_nels@mail.ru>
2023-04-12 14:24:28 +0300

    стр 136

--- app/src/main/java/com/example/tetrisnoactivity/models/AppModel.kt
+++ app/src/main/java/com/example/tetrisnoactivity/models/AppModel.kt
@@ -109,13 +109,29 @@ class AppModel {
                 }
 
             }
-            if (!moveValid((coordinate as Point, frameNumber)) {
+            if (!moveValid(coordinate as Point, frameNumber)) {
                 translateBlock(
                     currentBlock?.position as Point,
                     currentBlock?.frameNumber as Int
                 )
+                if (Motions.DOWN.name == action) {
+                    boostScore()
+                    persistCellData()
+                    assessField()
+                    generateNextBlock()
+                    if (!blockAdditionPosible()) {
+                        currentState = Statusus.OVER.name;
+                        currentBlock = null;
+                        resetField(false);
+                    }
+                }
 
-            })
+            } else {
+                if (frameNumber != null) {
+                    translateBlock(coordinate, frameNumber)
+                    currentBlock?.setState(frameNumber, coordinate)
+                }
+            }
 
         }
     }
@@ -127,4 +143,106 @@ class AppModel {
     enum class Motions {
         LEFT, RIGHT, DOWN, ROTATE
     }
+
+    private fun resetField(ephemeralCellsOnly: Boolean = true) {
+        for (i in 0 until FieldConstants.ROW_COUNT.value) {
+            (0 until FieldConstants.COLUMN_COUNT.value)
+                .filter { !ephemeralCellsOnly || field[i][it] == CellConstants.EPHEMERAL.value }
+                .forEach { field[i][it] = CellConstants.EMPTY.value }
+        }
+    }
+
+    private fun persistCellData() {
+        for (i in 0 until field.size) {
+            for (j in 0 until field[i].size) {
+                var status = getCellStatus(i, j)
+                if (status == CellConstants.EPHEMERAL.value) {
+                    status = currentBlock?.staticValue
+                    setCellStatus(i, j, status)
+                }
+            }
+        }
+    }
+
+    private fun assessField() {
+        for (i in 0 until field.size) {
+            var emptyCells = 0
+            for (j in 0 until field[i].size) {
+                val status = getCellStatus(i, j)
+                val isEmpty = CellConstants.EMPTY.value == status
+                if (isEmpty) emptyCells++
+            }
+            if (emptyCells == 0) shiftRows(i)
+        }
+    }
+
+    private fun translateBlock(position: Point, frameNumber: Int) {
+        synchronized(field) {
+            val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber)
+            if (shape != null) {
+                for (i in shape.indices) {
+                    for (j in 0 until shape[i].size) {
+                        val y = position.y + i
+                        val x = position.x + j
+                        if (CellConstants.EMPTY.value != shape[i][j]) {
+                            field[y][x] = shape[i][j]
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    private fun blockAdditionPosible(): Boolean {
+        if (!moveValid(
+                currentBlock?.position as Point,
+                currentBlock?.frameNumber
+            )
+        ) {
+            return false
+        }
+        return true
+    }
+
+    private fun shiftRows(nToRow: Int) {
+        if (nToRow > 0) {
+            for (j in nToRow - 1 downTo 0) {
+                for (m in 0 until field[j].size) {
+                    setCellStatus(j + 1, m, getCellStatus(j, m))
+                }
+            }
+            for (j in 0 until field[0].size) {
+                setCellStatus(0, j, CellConstants.EMPTY.value)
+            }
+        }
+    }
+    fun startGame(){
+        if (!isGameActive()) {
+            currentState = Statusus.ACTIVE.name
+            generateNextBlock()
+        }
+    }
+    fun restartGame() {
+        resetModel()
+        startGame()
+    }
+    fun endGame() {
+        score = 0
+        currentState = AppModel.Statusus.OVER.name
+    }
+    private fun resetModel() {
+        resetField(false)
+        currentState  = Statusus.AWAITING_START.name
+        score = 0
+    }
+
+    private fun blockAdditionPossible(): Boolean {
+        if (!moveValid(currentBlock?.position as Point,
+            currentBlock?.frameNumber)) {
+            return false
+        }
+        return true
+    }
+    
 }
